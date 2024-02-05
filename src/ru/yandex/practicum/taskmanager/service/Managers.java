8fe340015f77a2ca0171c8d4d49f4d59b10b752f
mrchcat <m-cat@mail.ru>
2024-02-05 22:42:50 +0300

    feat:  мелкие улучшения

--- src/ru/yandex/practicum/taskmanager/service/Managers.java
+++ src/ru/yandex/practicum/taskmanager/service/Managers.java
@@ -1,10 +1,8 @@
 package ru.yandex.practicum.taskmanager.service;
 
 import ru.yandex.practicum.taskmanager.repository.InMemoryBase;
-import ru.yandex.practicum.taskmanager.repository.Repository;
 import ru.yandex.practicum.taskmanager.tasks.Task;
 import ru.yandex.practicum.taskmanager.utils.CircularQueue;
-import ru.yandex.practicum.taskmanager.utils.Generator;
 import ru.yandex.practicum.taskmanager.utils.SerialGenerator;
 
 import java.util.ArrayList;
@@ -12,12 +10,12 @@ import java.util.ArrayList;
 public class Managers {
 
     private static final int HISTORY_SIZE = 10;
+    private static final int START_ID = 1;
     public static TaskManager getDefault() {
         var tasks = new InMemoryBase<Integer, Task>();
         var subordinates = new InMemoryBase<Integer, ArrayList<Integer>>();
-        Repository<Integer, Task> queueArray = new InMemoryBase<>();
-        CircularQueue<Task> queue = new CircularQueue<>(HISTORY_SIZE, queueArray);
-        Generator generator = new SerialGenerator(1);
+        var generator = new SerialGenerator(START_ID);
+        var queue = new CircularQueue<>(HISTORY_SIZE, new InMemoryBase<Integer, Task>());
         return new TaskManager(tasks, subordinates, generator, queue);
     }
 }
--- src/ru/yandex/practicum/taskmanager/utils/CircularQueue.java
+++ src/ru/yandex/practicum/taskmanager/utils/CircularQueue.java
@@ -41,8 +41,4 @@ public class CircularQueue<T> {
             return list;
         }
     }
-
-    public int getSize() {
-        return size;
-    }
 }
